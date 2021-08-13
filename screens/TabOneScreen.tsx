ae58ceeb3f02f021c3ae53a552a88cb547db9fbc
memo figueredo <memof80@gmail.com>
2021-08-13 13:14:46 -0500

    cells ready building in HomeScreen

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,32 +1,53 @@
 import * as React from 'react';
-import { StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
 
 export default function TabOneScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabOneScreen.tsx" />
-    </View>
+      <View style={styles.container}>
+          <Image 
+            source={{ uri: 'https://notjustdev-dummy.s3.us-east-2.amazonaws.com/avatars/elon.png'}}
+            style={styles.image}
+          />
+          <View style={styles.rightContainer}>
+            <View style={styles.row}>
+              <Text style={styles.name}>Elon Musk</Text>
+              <Text style={styles.text}>11:11 AM</Text>
+            </View>
+            <Text numberOfLines={1} style={styles.text}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime culpa magnam quibusdam atque velit laborum, totam cum fugit alias illum sapiente ea expedita quam corporis, deleniti illo minima quis suscipit?</Text>
+          </View>
+      </View>
   );
 }
 
+
 const styles = StyleSheet.create({
   container: {
+      flexDirection: 'row',
+      padding: 10,
+  },
+  image: {
+    width: 50,
+    height: 50,
+    borderRadius: 30,
+    marginRight: 10
+  }, 
+  rightContainer: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
-  title: {
-    fontSize: 20,
+  row: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  name: {
     fontWeight: 'bold',
+    fontSize: 18,
+    marginBottom: 3
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+  text: {
+    color: 'grey',
+    fontSize: 14
+  }
+})
+
