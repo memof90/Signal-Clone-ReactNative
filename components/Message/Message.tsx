b39d0c92d2284290ee6eb18bf884b7881a46d451
memo figueredo <memof80@gmail.com>
2021-08-14 18:10:04 -0500

    configuration Message components to render in chatroomScreen

---
+++ components/Message/Message.tsx
@@ -0,0 +1,21 @@
+//import liraries
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+// create a component
+const Message = () => {
+    return (
+        <View>
+            <Text>Message</Text>
+        </View>
+    );
+};
+
+// define your styles
+const styles = StyleSheet.create({
+
+});
+
+//make this component available to the app
+export default Message;
+
--- screens/ChatRoomScreen.tsx
+++ screens/ChatRoomScreen.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import { View, Text } from 'react-native'
+import Message from '../components/Message'
 
 export default function ChatRoomScreen() {
     return (
         <View>
-            <Text>Hello</Text>
+           <Message />
         </View>
     )
 }
