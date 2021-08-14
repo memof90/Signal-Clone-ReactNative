2e1b3f256c5c4661b1c7727eec171bb593415417
memo figueredo <memof80@gmail.com>
2021-08-14 18:42:08 -0500

    created effect messages

--- components/Message/Message.tsx
+++ components/Message/Message.tsx
@@ -2,18 +2,34 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+const blue = '#2E6DEF';
+const gray  = 'lightgrey';
+
 // create a component
 const Message = () => {
+
+    const isMe = true;
+
     return (
-        <View>
-            <Text>Message</Text>
+        <View style={[
+            styles.container, 
+            {backgroundColor: isMe ? gray : blue},
+            {marginLeft: isMe ? 'auto' :  10}
+        ]}>
+            <Text style={{color: isMe ? 'black' : 'white'}}>Message</Text>
         </View>
     );
 };
 
 // define your styles
 const styles = StyleSheet.create({
-
+    container: {
+        backgroundColor: '#2E6DEF',
+        padding: 10,
+        margin: 10,
+        borderRadius: 10,
+        maxWidth: '75%'
+    },
 });
 
 //make this component available to the app
